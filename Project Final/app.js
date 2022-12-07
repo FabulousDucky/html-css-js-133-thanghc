4baa93497524847e91dd30074d4e17345cc6113f
unknown <thanghc.piaget@gmail.com>
2022-12-07 23:32:44 +0700

    API

---
+++ Project Final/app.js
@@ -0,0 +1,33 @@
+function showInfo() {
+    console.log("Start call API")
+    let fetchRes = fetch("https://api.opendota.com/api/heroes");
+    console.log("End call API")
+    fetchRes.then(res =>
+        res.json()).then(d => {
+            const div = document.createElement("div");
+            div.innerHTML = `
+            <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Role</th>
+                    </tr>
+                    <tr>
+                        <td>${d[0].localized_name}</td>
+                        <td>${d[0].roles}</td>
+                    </tr>
+                    <tr>
+                        <td>${d[1].localized_name}</td>
+                        <td>${d[1].roles}</td>
+                    </tr>
+                </table>
+            `;
+            document.querySelector("body").appendChild(div);
+        })
+}
+
+// div.innerHTML = `
+//             <h1>${d[1].localized_name}</h1>
+//             <h2>${d[1].primary_attr}</h2>
+//             `;
+//             document.querySelector("body").appendChild(div);
+       
